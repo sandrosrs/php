fa8af95b51c81584684d313d4ba109497d2ca8bd
sandrosrs <60297238+sandrosrs@users.noreply.github.com>
2021-03-05 17:13:03 -0300

    teste

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-echo"Important
-Dessa maneira, fica claro que os textos podem ser curtos, com somente uma palavra, ou expresso por meio de um conjunto delas. Contudo, devemos ter atenção às características e os critérios essenciais de um texto.
-
-Sendo assim, o texto não é um emaranhado de frases, e para que ele seja efetivado existem dois critérios fundamentais: a coesão e coerência.";
+echo" dois critérios fundamentais: a coesão e coerência.";
 
 
 
