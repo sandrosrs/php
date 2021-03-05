44edc85e0b7b02e905e62104331298929ccac6e9
sandrosrs <60297238+sandrosrs@users.noreply.github.com>
2021-03-05 16:43:29 -0300

    teste

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-echo" Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ..";
+echo" O texto é uma manifestação escrita acerca das ideias de um autor (emissor ou locutor). Eles têm a função de transmitirem mensagens.
+
+Do grego, a palavra “texto” significa “tecido”. Assim, se pensarmos em sua dimensão etimológica, as palavras seriam os fios e o texto seria o tecido completo e organizado.
+
+Texto e Contexto.";
 
 
 
