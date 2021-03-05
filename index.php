cb2c11f76c37bd1e0db40dc3388299b7c23fbab7
sandrosrs <60297238+sandrosrs@users.noreply.github.com>
2021-03-05 17:10:17 -0300

    teste

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
-echo"Importante observar que o texto está intimamente ligado ao contexto, e somente existe quando essa relação se estabelece.
-
-Dessa forma, uma lista de supermercado é um texto, se, no entanto, para o leitor ela fizer sentido.
-
-Logo, se você encontra uma lista num ônibus, essa manifestação não será considerada um texto, uma vez que não faz sentido pra você, ou seja, está fora do seu contexto.
-
-Por outro lado, a palavra "silêncio" que surge nas paredes dos hospitais, está atrelado ao contexto e, por isso, é considerada um texto.
-
+echo"Important
 Dessa maneira, fica claro que os textos podem ser curtos, com somente uma palavra, ou expresso por meio de um conjunto delas. Contudo, devemos ter atenção às características e os critérios essenciais de um texto.
 
 Sendo assim, o texto não é um emaranhado de frases, e para que ele seja efetivado existem dois critérios fundamentais: a coesão e coerência.";
