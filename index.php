ead6efaabd93d02d3722c60001c67e7032ed3167
sandrosrs <60297238+sandrosrs@users.noreply.github.com>
2021-03-05 16:31:30 -0300

    teste

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo" Brasil dos Brasileirossssssssssssssssssssssssssssssssss";
+echo" Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ..";
 
 
 
