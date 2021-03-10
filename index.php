21a395e529e11a38f709c19e012a7233884f9815
sandrosrs <sandrorodriguess@mail.com>
2021-03-10 11:18:26 -0300

    mudança de estadoo

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-echo"sandro rodrigues ssss";
-=======
-echo"Aeto.Arojeto.";
->>>>>>> fe6b6cc76dedcb06e134b0b77a64e015bb99b2d5
-=======
-echo   "kkkkkkkkkkkkkkkkkkkkkk.";
->>>>>>> ccb67c7eca8226959a719fc94047104fc5a02c69
+
+
+echo"Brasilia df";
 
 
 
