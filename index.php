fe6b6cc76dedcb06e134b0b77a64e015bb99b2d5
sandrosrs <60297238+sandrosrs@users.noreply.github.com>
2021-03-10 10:04:40 -0300

    teste

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo"Agora precisamos linkar seu projeto com o seu repositório no github, pra isso você vai usar o comando git remote add origin https://github.com/user/repo.git. Você vai ter que mudar o link para o link do seu projeto, um projeto meu por exemplo é https://github.com/Vmussato/api-rest-node.git, use o link do seu projeto.Agora precisamos linkar seu projeto com o seu repositório no github, pra isso você vai usar o comando git remote add origin https://github.com/user/repo.git. Você vai ter que mudar o link para o link do seu projeto, um projeto meu por exemplo é https://github.com/Vmussato/api-rest-node.git, use o link do seu projeto.";
+echo"Aeto.Arojeto.";
 
 
 
